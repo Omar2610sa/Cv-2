de8ab111f60e0cf6f10a12acc7e420c5d85d15ae
Omar Salam <omarmooataz26@gmail.com>
2025-12-24 14:56:35 +0200

    Update the structure of scrollTo Top

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,33 @@
-window.addEventListener("scroll", function () {
-    const navbar = document.querySelector(".nav-bar");
 
-    if (window.scrollY > 150) { // لما تنزل أكتر من 100px
-        navbar.classList.add("show");
-    } else {
-        navbar.classList.remove("show");
-    }
+/*
+CLick to Humburger
+*/
+const navbar  = document.querySelector("#navv");
+const bars = document.querySelector(".fa-bars");
+const xmark = document.querySelector(".fa-x");
+const hum = document.querySelector(".humburger");
+
+hum.addEventListener("click", () => {
+    bars.classList.toggle("active");
+    xmark.classList.toggle("active");
+    navbar.classList.toggle("active");
 });
+
+// Scroll to top function 
+let scrollTop = document.querySelector(".scroll-to-top")
+// Add clss show for visability
+window.addEventListener("scroll",()=>{
+if (window.scrollY >=200){
+    scrollTop.classList.add("show")
+}else
+{
+    scrollTop.classList.remove("show")
+}
+})
+// Add action click to top
+scrollTop.onclick = function(){
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    })
+}
