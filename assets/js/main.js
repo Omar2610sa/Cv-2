a019b6ffd6de22b877500ff3afbba19a33413f5f
Omar Salam <omarmooataz26@gmail.com>
2025-12-30 12:59:25 +0200

    Update: Validate the form

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,3 +32,69 @@ scrollTop.onclick = function(){
     })
     scrollTop.classList.toggle("toggle")
 }
+
+// Contact Validation
+
+const form = document.querySelector("#contact-form")
+const username = document.getElementById("name")
+const email = document.getElementById("email")
+const phone = document.getElementById("phone")
+const message = document.getElementById("message")
+
+// Show Error
+function showError(input, message){
+    const formControl = input.parentElement
+    formControl.className = "form-control error"
+    const span = formControl.querySelector("span");
+    span.innerText = message
+}
+
+// Show Sucsses
+function showSuccess(input){
+    const formControl = input.parentElement
+    formControl.className = "form-control"
+
+}
+
+form.addEventListener("submit", (e)=>{
+
+    let isValid = true
+    // User Name
+    if(username.value.trim() === ""){
+        showError(username, "Username is requried")
+        isValid = false
+    } else {
+        showSuccess(username)
+    }
+    // Email Validatiom
+    if(email.value.trim() === ""){
+        showError(email, "Email is requried")
+        isValid = false
+
+    } else {
+        showSuccess(email)
+    }
+    // Phone Validation
+    if(phone.value.trim() === ""){
+        showError(phone, "Phone is requried")
+        isValid = false
+    } else if (phone.value.trim().length !== 11){
+        showError(phone, "Enter a valid phone number with 11 numbers")
+        isValid = false
+    } 
+    else {
+        showSuccess(phone)
+    }
+    // message Validatiom
+    if(message.value.trim() === ""){
+        showError(message, "Message is requried")
+        isValid = false
+    } else {
+        showSuccess(message)
+    }
+
+
+    if(isValid === false){
+        e.preventDefault()
+    }
+})
